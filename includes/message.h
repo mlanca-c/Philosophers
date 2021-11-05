a9f86513ff5bbd55335dd39d01dcb8cce6e2add9
MLSTT <marialsottomayor@gmail.com>
2021-11-05 20:17:10 +0000

    Added nem messages to header

--- includes/message.h
+++ includes/message.h
@@ -6,14 +6,18 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:52:39 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/11/04 19:54:06 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/11/05 20:03:33 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MESSAGE_H
 # define MESSAGE_H
 
-# define MALLOC_ERROR	"failed at allocating memory.\n"
-# define INVALID_ARGS	"invalid arguments.\n"
+# define MALLOC_ERROR		"failed at allocating memory.\n"
+# define INVALID_ARGS_NUM	"invalid arguments, wrong number of arguments\n"
+# define INVALID_ARGS_MINUS	"invalid arguments, using negative numbers.\n"
+# define INVALID_ARGS_ALPHA	"invalid arguments, using non-integers arguments.\n"
+# define MUTEX_ERROR		"failed at creating new mutex.\n"
+# define THREAD_ERROR		"failed at creating new thread.\n"
 
 #endif //MESSAGE_H
