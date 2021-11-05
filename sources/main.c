948176f3a166e816afe91f10f9106967d1b533b5
MLSTT <marialsottomayor@gmail.com>
2021-11-05 20:20:41 +0000

    Updated main.c Added init and parsing error features

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:38:41 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/11/04 20:13:35 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/11/05 20:14:47 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,13 @@
 */
 int	main(int argc, char *argv[])
 {
+	t_ctrl	*control;
+
 	if (argc > 1 && !ft_strcmp(argv[1], "--help"))
 		help_message();
-	else if (argc < 5)
-		error_message(INVALID_ARGS);
+	else if (argc != 5 && argc != 6)
+		error_message(INVALID_ARGS_NUM);
+	control = init_control(argc, argv);
+	exit_program(control);
 	return (0);
 }
