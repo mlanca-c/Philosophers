01cf014a3344651bcb34b9bec0a6b295dd195ef7
MLSTT <marialsottomayor@gmail.com>
2021-11-05 20:19:06 +0000

    Update file with colors Added the color output feature to the error oututs the user might have

--- sources/command_utils.c
+++ sources/command_utils.c
@@ -6,36 +6,56 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 20:15:53 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/11/04 20:35:32 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/11/05 20:07:42 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
 /*
+** This function is called if the command line argument is ./philo --help.
+** This will display to the user the desired input for the program. The function
+** then exits the program with EXIT_SUCCESS.
 */
 void	help_message(void)
 {
-	printf("[ format ]: ");
+	printf("\n[ " ANSI_GREEN "ok" ANSI_RESET " ]:\t\t");
+	printf("./philo 1 800 200 200 7\n");
+	printf("[ " ANSI_GREEN "format" ANSI_RESET " ]:\t");
 	printf("./philo <num_philo> <time_to_die> <time_to_eat> ");
-	printf("<time_to_sleep> [num_times_each_philo_must_eat]\n\n");
-	printf("\t* num_philo: number of philosophers (mandatory).\n");
-	printf("\t* time_to_die: time a philosopher can go without eating\n");
-	printf("\t\tcounted from the moment he started eating (mandatory).\n");
-	printf("\t* time_to_eat: (mandatory)\n");
-	printf("\t* time_to_sleep: (mandatory)\n");
-	printf("\t* num_times_each_philo_must_eat: (optional)\n");
+	printf("<time_to_sleep> [num_times_each_philo_must_eat]\n");
 	exit(EXIT_SUCCESS);
 }
 
 /*
+** This function is called if the command line arguments given by the user are
+** not in the correct format:
+**		./philo <num_philo> <time_to_die> <time_to_eat> <time_to_sleep>
+**										[num_times_each_philo_must_eat]
+**
+** If so, the error_message() function displays an error message, and exits the
+** program with EXIT_FAILURE.
+** This function takes no arguments and ends the program when called.
+**
+** @param	char	*message	- message to be displayed to the user.
 */
 void	error_message(char *message)
 {
-	printf("[ philosophers ]: %s", message);
-	printf("[ correct format ]:\n");
-	printf("\t./philo <num_philo> <time_to_die> <time_to_eat> ");
-	printf("<time_to_sleep> [num_times_each_philo_must_eat]\n");
-	printf("\n\tadditional info: ./philo --help\n");
+	printf("\n[ " ANSI_RED "philosophers" ANSI_RESET " ]: %s", message);
+	printf("[ "ANSI_RED "info" ANSI_RESET " ]: ./philo --help\n");
 	exit(EXIT_FAILURE);
+}
+
+/*
+** This function is called once the simulation ends - either a philosopher dies
+** or every philosopher ate the maximum amount of times it could.
+** The function frees all allocated memory and then exits with EXIT_SUCCESS.
+**
+** @param	t_ctrl	*control	- main struct of the program. Some of the
+** 								variables it contains need to be freed.
+*/
+void	exit_program(t_ctrl *control)
+{
+	free(control);
+	exit(EXIT_SUCCESS);
 }
