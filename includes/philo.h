6b18583ba1f752d49dc9d5121795abd27c59dc99
MLSTT <marialsottomayor@gmail.com>
2021-11-05 20:18:11 +0000

    Update header philo with new struct params and new include files remade some of the structures corrected norm errors added new include header files such as message.h

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:25:56 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/11/04 20:35:47 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/11/05 20:08:39 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,35 +17,45 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <stdbool.h>
+# include "ansi.h"
 # include "message.h"
 
 /*
 */
-typedef enum e_state
+typedef enum e_bool
+{
+	TRUE,
+	FALSE
+}	t_bool;
+
+/*
+*/
+typedef enum e_status
 {
 	EATING,
 	SLEEPING,
 	THINKING,
 	FINISHED
-}	t_state;
+}	t_status;
 
+/*
+*/
 typedef struct s_fork
 {
 	int				philo_name;
 	bool			used;
-	pthread_mutex_t mutex;
-
-} t_fork;
+	pthread_mutex_t	mutex;
+}	t_fork;
 
 /*
 */
 typedef struct s_philo
 {
 	int					name;
-	t_state				state;
-	//pthread_t			thread;
-	//t_fork			right_fork;
-	//t_fork			left_fork;
+	t_status			status;
+	pthread_t			thread;
+	t_fork				right_fork;
+	t_fork				left_fork;
 	int					times_philo_ate;
 	struct s_control	*control;
 }	t_philo;
@@ -54,21 +64,32 @@ typedef struct s_philo
 */
 typedef struct s_control
 {
+	int		nu_of_philo;
 	int		time_to_die;
 	int		time_to_eat;
 	int		time_to_sleep;
-	int		time_to_eat_num;
-	t_philo *philos;
-} t_ctrl;
+	int		nu_of_time_to_eat;
+	t_philo	*philos;
+	t_fork	*forks;
+}	t_ctrl;
 
 /*
-*/ 
+** command_utils.c Functions
+*/
 void	help_message(void);
 void	error_message(char *message);
+void	exit_program(t_ctrl *control);
 
 /*
-*/ 
+**  mini_libft.c Functions
+*/
 void	*ft_malloc(int size, void (*error_message)(char *));
 int		ft_strcmp(char *s1, char *s2);
+int		ft_atoi(const char *str);
+
+/*
+** control.c Functions
+*/
+t_ctrl	*init_control(int argc, char **argv);
 
 #endif //PHILO_H
