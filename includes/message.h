facbd327b81d3fd1033646fb82d342231a2390d6
mlanca-c <mlanca-c@student.42lisboa.com>
2021-11-16 21:22:22 +0000

    Update header files Last header touches. Upgrade project's structure in philo.h

--- includes/message.h
+++ includes/message.h
@@ -6,7 +6,7 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 19:52:39 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/11/16 20:24:23 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/11/16 21:22:10 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,13 @@
 /*
 ** Error messages for error_message() function.
 */
-# define MALLOC_ERROR		"failed at allocating memory.\n"
 # define INVALID_ARGS_NUM	"invalid arguments, wrong number of arguments\n"
 # define INVALID_ARGS_MINUS	"invalid arguments, using negative numbers.\n"
 # define INVALID_ARGS_ALPHA	"invalid arguments, using non-integers arguments.\n"
+# define WRONG_PHILO_NU		"invalid argumenst, wrong number of philosophers.\n"
+# define MALLOC_ERROR		"failed at allocating memory.\n"
 # define MUTEX_ERROR		"failed at creating new mutex.\n"
 # define THREAD_ERROR		"failed at creating new thread.\n"
-# define NO_PHILO			"program needs to have at least one philosopher.\n"
-# define TOO_MANY_PHILO		"program can't have more than 200 philosophers.\n"
 
 /*
 ** Simulation messages
